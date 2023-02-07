f15cef55ffe2de7e25ee8731ab79e33cbc5f8e71
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-07 12:30:05 -0300

    🎨 copyright logo size & align

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,10 +1,14 @@
 import { SocialMedia } from '../SocialMedia/SocialMedia'
 
-const Footer = () => (
-  <footer className='footer'>
-    <SocialMedia />
-    <p className='footer__copy'>2023 - {new Date().getFullYear()} &copy; All rights reserved</p>
-  </footer>
-)
+const Footer = () => {
+  const actualYear = new Date().getFullYear()
+
+  return (
+    <footer className='footer' id='bottom'>
+      <SocialMedia />
+      <p className='footer__copy'>{actualYear === 2023 ? '2023' : `2023 - ${actualYear}`} <span className='footer__copy--icon'>&copy;</span> All rights reserved</p>
+    </footer>
+  )
+}
 
 export { Footer }

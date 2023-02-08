f22b94597d189364bcb06312e21ae10e13cad0f4
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-08 18:59:45 -0300

    🦶🏻 using langcontext in footer to change copyright lang

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,12 +1,15 @@
+import { useContext } from 'react'
 import { SocialMedia } from '../SocialMedia/SocialMedia'
+import LangContext from '../../context/LangContext'
 
 const Footer = () => {
+  const { translations } = useContext(LangContext)
   const actualYear = new Date().getFullYear()
 
   return (
     <footer className='footer' id='bottom'>
       <SocialMedia />
-      <p className='footer__copy'>{actualYear === 2023 ? '2023' : `2023 - ${actualYear}`} <span className='footer__copy--icon'>&copy;</span> All rights reserved</p>
+      <p className='footer__copy'>{actualYear === 2023 ? '2023' : `2023 - ${actualYear}`} <span className='footer__copy--icon'>&copy;</span>{translations.footer.copy}</p>
     </footer>
   )
 }

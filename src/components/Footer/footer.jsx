6e14e17bdae6823001237bf5f8bcc4ea076f2405
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-14 14:29:57 -0300

    🪄 installed prettier

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,9 +7,16 @@ const Footer = () => {
   const actualYear = new Date().getFullYear()
 
   return (
-    <footer className='footer' id='bottom'>
+    <footer
+      className='footer'
+      id='bottom'
+    >
       <SocialMedia />
-      <p className='footer__copy'>{actualYear === 2023 ? '2023' : `2023 - ${actualYear}`} <span className='footer__copy--icon'>&copy;</span>{translations.footer.copy}</p>
+      <p className='footer__copy'>
+        {actualYear === 2023 ? '2023' : `2023 - ${actualYear}`}{' '}
+        <span className='footer__copy--icon'>&copy;</span>
+        {translations.footer.copy}
+      </p>
     </footer>
   )
 }

1260391ca95d2c434c9053305d9a2d10f104691d
Facundo Carrion <facundojcarrion@gmail.com>
2023-03-12 11:11:19 -0400

    rename footer component and remove strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './sass/index.scss'
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+root.render(<App />)

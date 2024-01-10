5c52d06c1e089492690532c21231ecade491c41e
Facundo Carrion <facundojcarrion@gmail.com>
2024-01-10 13:58:31 -0300

    feat: main structure and main content

---
+++ src/data/tags.data.ts
@@ -0,0 +1,59 @@
+import {
+  HtmlIcon,
+  CssIcon,
+  JavaScriptIcon,
+  AstroIcon,
+  SvelteIcon,
+  TailWindIcon,
+  SqlIcon,
+  ReactIcon,
+  NodeIcon
+} from '../components/icons'
+
+export default {
+  HTML: {
+    text: 'HTML',
+    icon: HtmlIcon,
+    class: 'bg-[#963c0c]/80 border-[#e96228] border-2 text-[#e96228]'
+  },
+  CSS: {
+    text: 'CSS',
+    icon: CssIcon,
+    class: 'bg-[#0c3c96]/80 border-[#2862e9] border-2 text-[#2862e9]'
+  },
+  JavaScript: {
+    text: 'JavaScript',
+    icon: JavaScriptIcon,
+    class: 'bg-[#9d9200]/80 border-[#f7e025] border-2 text-[#f7e025]'
+  },
+  Astro: {
+    text: 'Astro',
+    icon: AstroIcon,
+    class: 'bg-[#090909]/80 border-[#ffffff] border-2 text-[#ffffff]'
+  },
+  Svelte: {
+    text: 'Svelte',
+    icon: SvelteIcon,
+    class: 'bg-[#990c00]/80 border-[#ff3e00] border-2 text-[#ff3e00]'
+  },
+  Tailwind: {
+    text: 'TailwindCSS',
+    icon: TailWindIcon,
+    class: 'bg-[#003159]/80 border-[#06b6d4] border-2 text-[#06b6d4]'
+  },
+  Sql: {
+    text: 'MySQL',
+    icon: SqlIcon,
+    class: 'bg-[#044c63]/80 border-[#0b82b5] border-2 text-[#0b82b5]'
+  },
+  React: {
+    text: 'React',
+    icon: ReactIcon,
+    class: 'bg-[#2082a8]/80 border-[#67defe] border-2 text-[#67defe]'
+  },
+  Node: {
+    text: 'NodeJS',
+    icon: NodeIcon,
+    class: 'bg-[#4a843b]/80 border-[#74be55] border-2 text-[#74be55]'
+  }
+}
